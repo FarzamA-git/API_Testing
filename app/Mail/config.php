3c5d1ffe52ff005210bed594f857dbb22794f00e
FarzamA-git <89745763+FarzamA-git@users.noreply.github.com>
2021-08-29 23:47:33 +0500

    API_TESTS

--- app/Mail/config.php
+++ app/Mail/config.php
@@ -5,12 +5,12 @@ return [
     'host' => env('MAIL_HOST', 'smtp.gmail.org'),
     'port' => env('MAIL_PORT', 465),
     'from' => [
-        'address' => env('farzamamjad0@gmail.com', 'farzamamjad0@gmail.com'),
+        'address' => env('sample@gmail.com', 'sample@gmail.com'),
         'name' => env('Farzam', 'Farzam'),
     ],
     'encryption' => env('ssl', 'ssl'),
-    'username' => env('farzamamjad0Gmail.com'),
-    'password' => env('Fa03200489980'),
+    'username' => env('Enter your name'),
+    'password' => env('Enter your password'),
     'sendmail' => '/usr/sbin/sendmail -bs',
     'markdown' => [
         'theme' => 'default',
--- routes/postApi.php
+++ routes/postApi.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::POST('/create','PostController@create');
 Route::GET('/websites','PostController@websites');
 Route::GET('show/{id}','PostController@show');
 Route::Delete('delete/{id}', 'PostController@delete');
